6811bd6550b02ac172155a86b252be7f0ef289c4
Jose <jeduramos7001@gmail.com>
2026-03-29 02:42:15 -0700

    Add ExampleSwiftUI project

---
+++ ExampleSwiftUI/ExampleSwiftUI/ExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIApp.swift
+//  ExampleSwiftUI
+//
+//  Created by Jose Ramos on 29/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
