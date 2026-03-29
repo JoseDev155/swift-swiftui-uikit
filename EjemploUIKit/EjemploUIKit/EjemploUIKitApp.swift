afe9168ef5f562d41c2b4059421b1e1d0d2fbd5d
Jose <jeduramos7001@gmail.com>
2026-03-29 02:29:24 -0700

    Add EjemploUIKit project

---
+++ EjemploUIKit/EjemploUIKit/EjemploUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjemploUIKitApp.swift
+//  EjemploUIKit
+//
+//  Created by Jose Ramos on 26/3/26.
+//
+
+import SwiftUI
+
+@main
+struct EjemploUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
